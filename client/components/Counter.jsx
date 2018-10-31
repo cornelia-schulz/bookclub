03972e82a97ceb5b225b749b75431aed91919f23
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-10-31 15:41:18 +1300

    added custom hook useWindowWidth

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import i18n from 'i18next'
 import { withNamespaces } from 'react-i18next'
+import { useWindowWidth } from './UseWindowWidth'
 
 function Counter(props) {
     let { t, i18n } = props
     const [count, setCount] = useState(0)
+    const width = useWindowWidth()
 
     return (
         <div className='counter'>
             <button onClick={() => setCount(count + 1)}>Click me</button>
             <p>{t('Counter.click', {count})}</p>
+            <p>Window width is {width}</p>
         </div>
     )
 }
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -295,6 +295,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var i18next__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! i18next */ "./node_modules/i18next/dist/es/index.js");
 /* harmony import */ var react_i18next__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-i18next */ "./node_modules/react-i18next/dist/es/index.js");
+/* harmony import */ var _UseWindowWidth__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./UseWindowWidth */ "./client/components/UseWindowWidth.jsx");
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
@@ -302,6 +303,7 @@ function _nonIterableRest() { throw new TypeError("Invalid attempt to destructur
 function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 
@@ -317,6 +319,7 @@ function Counter(props) {
       count = _useState2[0],
       setCount = _useState2[1];
 
+  var width = Object(_UseWindowWidth__WEBPACK_IMPORTED_MODULE_3__["useWindowWidth"])();
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "counter"
   }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
@@ -325,7 +328,7 @@ function Counter(props) {
     }
   }, "Click me"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, t('Counter.click', {
     count: count
-  })));
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Window width is ", width));
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (Object(react_i18next__WEBPACK_IMPORTED_MODULE_2__["withNamespaces"])('strings')(Counter));
@@ -380,6 +383,48 @@ var Header = function Header(props) {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Object(react_i18next__WEBPACK_IMPORTED_MODULE_2__["withNamespaces"])('strings')(Header));
+
+/***/ }),
+
+/***/ "./client/components/UseWindowWidth.jsx":
+/*!**********************************************!*\
+  !*** ./client/components/UseWindowWidth.jsx ***!
+  \**********************************************/
+/*! exports provided: useWindowWidth */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "useWindowWidth", function() { return useWindowWidth; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+
+function useWindowWidth() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(window.innerWidth),
+      _useState2 = _slicedToArray(_useState, 2),
+      width = _useState2[0],
+      setWidth = _useState2[1];
+
+  Object(react__WEBPACK_IMPORTED_MODULE_0__["useEffect"])(function () {
+    var handleResize = function handleResize() {
+      return setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return function () {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+  return width;
+}
 
 /***/ }),
 
